7ca4dbb6c52e88c6e160a6812f1216b4f37f29f4
= <saad@focusteck.com>
2018-11-08 20:05:11 +0500

    commit

---
+++ app/src/androidTest/java/speech/ft/com/android_speech/ApplicationTest.java
@@ -0,0 +1,10 @@
+package speech.ft.com.android_speech;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
